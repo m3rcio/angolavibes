9a3731f37ec0f2d3996d312f7b490d1dbfc173ba
Mércio <mercio20a@gmail.com>
2026-02-01 23:48:09 +0100

    botão buscar

--- angolavibes/src/components/BotaoBuscar.tsx
+++ angolavibes/src/components/BotaoBuscar.tsx
@@ -5,3 +5,20 @@ type BotaoBuscarProps={
     loading?: boolean;
 };
 
+const BotaoBuscar: React.FC<BotaoBuscarProps>=({onBuscar,loading})=>{
+    return(
+         <button
+      type="button"
+      onClick={onBuscar}
+      disabled={loading}
+      style={{
+        padding: "8px 16px",
+        cursor: loading ? "not-allowed" : "pointer",
+      }}
+    >
+      {loading ? "Buscando..." : "Buscar"}
+    </button>
+    );
+};
+
+export default BotaoBuscar;
