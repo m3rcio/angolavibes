dd4805ab40d9a6f67f629e82a880dca03c85df4b
Mércio <mercio20a@gmail.com>
2026-02-09 11:33:48 +0100

    trabalhando em fazer o signup funcionar

--- angolavibes/src/components/SignupModal/SignupModal.tsx
+++ angolavibes/src/components/SignupModal/SignupModal.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./SignupModal.css";
 
 type SignupModalProps = {
@@ -5,7 +6,54 @@ type SignupModalProps = {
   onClose: () => void;
 };
 
+
+
 export default function SignupModal({ isOpen, onClose }: SignupModalProps) {
+const [nome,setNome]=useState("");
+const [email,setEmail]=useState("");
+const [senha,setSenha]=useState("");
+const [loading,setLoading]=useState(false);
+const [error,setError]=useState("");
+  
+  
+  async function handleSignup() {
+    setError("");
+
+    if (!nome || !email || !senha) {
+      setError("Preencha todos os campos");
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const response = await fetch("http://localhost:5000/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          nome,
+          email,
+          senha
+        })
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        setError(data.message || "Erro ao criar conta");
+        return;
+      }
+
+      alert("Conta criada com sucesso");
+      
+    } catch (err) {
+      setError("Erro de conexão com o servidor");
+    } finally {
+      setLoading(false);
+    }
+  }
   if (!isOpen) return null;
 
   return (
@@ -42,23 +90,28 @@ export default function SignupModal({ isOpen, onClose }: SignupModalProps) {
         <input
           type="text"
           placeholder="Nome"
-          className="signup-input"
+          className="signup-input" value={nome}
+          onChange={(e)=>setNome(e.target.value)}
         />
 
         <input
           type="email"
           placeholder="Email"
-          className="signup-input"
+          className="signup-input" value={email}
+          onChange={(e)=>setEmail(e.target.value)}
         />
 
         <input
           type="password"
           placeholder="Senha"
-          className="signup-input"
+          className="signup-input" value={senha}
+          onChange={(e)=>setSenha(e.target.value)}
         />
+        {error && <p className="error-text">{error}</p>}
 
-        <button className="btn-criar">Criar conta</button>
-
+        <button className="btn-criar" onClick={handleSignup}
+        disabled={loading}>{loading ? "Criando..." : "Criar conta"}</button>
+        
         <p className="login-redirect">
           Já tem conta? <a href="#">Entrar</a>
         </p>
