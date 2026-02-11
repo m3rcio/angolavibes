1cce8a1325ec856d72b3a7706a62db8afa22d19d
unknown <mercio20a@gmail.com>
2026-02-11 19:33:37 -0300

    adicionado login no routes.ts

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { signup } from "../controllers/auth.controller";
+import { login } from "../controllers/auth.controller";
 import {googleAuth} from "../controllers/auth.controller"
 const authRoutes=Router();
 
 authRoutes.post("/signup",signup);
+authRoutes.post("/login",login);
 authRoutes.post("/google", googleAuth);
 export default authRoutes;
