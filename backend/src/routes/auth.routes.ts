62eb51162068928bd77716451d82d3c9fa985931
unknown <mercio20a@gmail.com>
2026-02-12 01:34:18 -0300

    minor changes

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 import { signup } from "../controllers/auth.controller";
 import { login } from "../controllers/auth.controller";
 import {googleAuth} from "../controllers/auth.controller"
+import { refreshTokenController } from "../controllers/utils/refreshTokenController";
 const authRoutes=Router();
 
 authRoutes.post("/signup",signup);
 authRoutes.post("/login",login);
+authRoutes.post("/refresh",refreshTokenController);
 authRoutes.post("/google", googleAuth);
 export default authRoutes;
