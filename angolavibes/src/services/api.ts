b5e8ebceb8704f339f8295da2af72fdaeb37f450
unknown <mercio20a@gmail.com>
2026-02-12 16:29:28 -0300

    ...

--- angolavibes/src/services/api.ts
+++ angolavibes/src/services/api.ts
@@ -1,5 +1,41 @@
-import axios from axios;
+import axios from "axios";
 
 const api= axios.create({
-    baseUrl:"http://localhost:5000"
-})
+    baseURL:"http://localhost:5000"
+});
+
+api.interceptors.request.use(config => {
+    const token= localStorage.getItem("accessToken");
+    if(token){
+        config.headers.Authorization=`Bearer ${token}`;
+    }return config;
+});
+
+api.interceptors.response.use(
+  response => response,
+  async error => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      const refreshToken = localStorage.getItem("refreshToken");
+
+      const res = await axios.post("http://localhost:5000/refresh", {
+        refreshToken
+      });
+
+      localStorage.setItem("accessToken", res.data.accessToken);
+
+      originalRequest.headers.Authorization =
+        `Bearer ${res.data.accessToken}`;
+
+      return api(originalRequest);
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+
+export default api;
