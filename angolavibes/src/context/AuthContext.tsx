48550d608a7328ab3e30a81d39c863764fcc3892
unknown <mercio20a@gmail.com>
2026-02-16 23:08:03 -0300

    excluído comentário desnecessário

--- angolavibes/src/context/AuthContext.tsx
+++ angolavibes/src/context/AuthContext.tsx
@@ -6,7 +6,7 @@ export const AuthContext = createContext<any>(null);
 export const AuthProvider = ({children}:any)=>{
   const [user,setUser] = useState(null);
 
-  const login = async (email:string,senha:string)=>{
+  const login= async (email:string,senha:string)=>{
     
     try{const res = await api.post("http://localhost:5000/api/auth/login",{email,senha});
     setAccessToken(res.data.accessToken);
@@ -45,46 +45,3 @@ export const AuthProvider = ({children}:any)=>{
 };
 
 
-// import { createContext, useContext, useState, useEffect } from "react";
-
-// const AuthContext = createContext();
-
-// export function AuthProvider({ children }) {
-//   const [user, setUser] = useState(null);
-//   const [loading, setLoading] = useState(true);
-
-//      useEffect(() => {
-//     const token = localStorage.getItem("accessToken");
-
-//     if (token) {
-//       // opcional: decodificar ou buscar perfil no backend
-//       setUser({ logged: true });
-//     }
-
-//     setLoading(false);
-//   }, []);
-
-//    function login(userData, accessToken, refreshToken) {
-//     localStorage.setItem("accessToken", accessToken);
-//     localStorage.setItem("refreshToken", refreshToken);
-//     setUser(userData);
-//   }
-
-//    function logout() {
-//     localStorage.removeItem("accessToken");
-//     localStorage.removeItem("refreshToken");
-//     setUser(null);
-//   }
-
- 
-
-//    return (
-//     <AuthContext.Provider value={{ user, login, logout, loading }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// }
-
-//  export function useAuth() {
-//   return useContext(AuthContext);
-// }
