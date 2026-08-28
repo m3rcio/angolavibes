39808384ab9ac89b1f96ec18bf2506d67ab8a819
unknown <mercio20a@gmail.com>
2026-08-28 10:28:33 -0300

    comentado código do controller de lugares temporáriamente

--- backend/src/controllers/lugarQuery.controller.ts
+++ backend/src/controllers/lugarQuery.controller.ts
@@ -22,62 +22,62 @@ interface LugarJoinRow extends RowDataPacket {
 }
 
 
- export async function buscarLugares((req, res) => {
-  try {
-    const { query, categoria, pageToken } = req.query;
-// lógica para validar query e categoria vem aqui
-const isQueryValid=validarBusca(query);
+//  export async function buscarLugares((req, res) => {
+//   try {
+//     const { query, categoria, pageToken } = req.query;
+// // lógica para validar query e categoria vem aqui
+// const isQueryValid=validarBusca(query);
 
-if(isQueryValid.isValid==true){
-  buscarLugar(query,categoria,pageToken)
-}
+// if(isQueryValid.isValid==true){
+//   buscarLugar(query,categoria,pageToken)
+// }
    
-   // mostrar lugares atravez da busca
-    const [rows] = await db.query<LugarJoinRow[]>(`
-      SELECT 
-        l.id,
-        l.nome,
-        l.descricao,
-        l.google_place_id,
-        l.endereco,
-        l.latitude,
-        l.longitude,
-        l.telefone,
-        l.preco_medio,
-        li.imagem_url
-      FROM lugares l
-      LEFT JOIN lugar_imagens li 
-        ON li.lugar_id = l.id
-        WHERE l.nome LIKE ?
-        `, [`%${query}%`]);
+//    // mostrar lugares atravez da busca
+//     const [rows] = await db.query<LugarJoinRow[]>(`
+//       SELECT 
+//         l.id,
+//         l.nome,
+//         l.descricao,
+//         l.google_place_id,
+//         l.endereco,
+//         l.latitude,
+//         l.longitude,
+//         l.telefone,
+//         l.preco_medio,
+//         li.imagem_url
+//       FROM lugares l
+//       LEFT JOIN lugar_imagens li 
+//         ON li.lugar_id = l.id
+//         WHERE l.nome LIKE ?
+//         `, [`%${query}%`]);
 
-    const lugaresMap = new Map<number, any>();
-    rows.forEach((row) => {
-      if (!lugaresMap.has(row.id)) {
-        lugaresMap.set(row.id, {
-          id: row.id,
-          nome: row.nome,
-          descricao: row.descricao,
-          google_place_id: row.google_place_id,
-          endereco: row.endereco,
-          latitude: row.latitude,
-          longitude: row.longitude,
-          telefone: row.telefone,
-          preco_medio: row.preco_medio,
-          imagens: []
-        });
-      }
+//     const lugaresMap = new Map<number, any>();
+//     rows.forEach((row) => {
+//       if (!lugaresMap.has(row.id)) {
+//         lugaresMap.set(row.id, {
+//           id: row.id,
+//           nome: row.nome,
+//           descricao: row.descricao,
+//           google_place_id: row.google_place_id,
+//           endereco: row.endereco,
+//           latitude: row.latitude,
+//           longitude: row.longitude,
+//           telefone: row.telefone,
+//           preco_medio: row.preco_medio,
+//           imagens: []
+//         });
+//       }
 
-      if (row.imagem_url) {
-        lugaresMap.get(row.id).imagens.push(row.imagem_url);
-      }
-    });
+//       if (row.imagem_url) {
+//         lugaresMap.get(row.id).imagens.push(row.imagem_url);
+//       }
+//     });
 
-    const lugaresJoin = Array.from(lugaresMap.values());
-    res.json(lugaresJoin);
+//     const lugaresJoin = Array.from(lugaresMap.values());
+//     res.json(lugaresJoin);
 
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Erro ao buscar lugares" });
-  }
-});
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Erro ao buscar lugares" });
+//   }
+// });
